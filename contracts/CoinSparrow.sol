039621ca29c5149c20df9f8fa3ab451570aa6585
Codegnosis <paul@codegnosis.co.uk>
2018-06-18 13:26:17 +0100

    added doc/comments to getJobHash function

--- contracts/CoinSparrow.sol
+++ contracts/CoinSparrow.sol
@@ -1220,6 +1220,15 @@ contract CoinSparrow  is Ownable, Arbitrator, ApprovedWithdrawer, Pausable {
     return addr == _signer;
   }
 
+  /**
+   * @dev Generates the sha256 jobHash based on job parameters. Used in several functions
+   * @param _jobId The unique ID of the job, from the CoinSparrow database
+   * @param _hirer The wallet address of the hiring (buying) party
+   * @param _contractor The wallet address of the contractor (selling) party
+   * @param _value The ether amount being held in escrow. I.e. job cost - amount hirer is paying contractor
+   * @param _fee CoinSparrow fee for this job. Pre-calculated
+   * @return bytes32 the calculated jobHash value
+   */
   function getJobHash(
     bytes16 _jobId,
     address _hirer,
